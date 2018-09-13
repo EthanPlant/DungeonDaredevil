e1aa82ca39c00a781f4934d4397a0afa4170f327
churj8630 <churj8630@wrdsb.ca>
2018-09-13 14:30:41 -0400

    red map

--- core/src/pjkck/dungeondaredevil/screens/ScrMap.java
+++ core/src/pjkck/dungeondaredevil/screens/ScrMap.java
@@ -24,7 +24,7 @@ public class ScrMap implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     }

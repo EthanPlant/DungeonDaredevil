745317a565e5f58d1153e6e15c7bc1e2fcacff31
Ethan Plant <plant.ethan@gmail.com>
2018-09-14 13:32:14 -0400

    Sprite added and moving

--- core/src/pjkck/dungeondaredevil/screens/ScrMap.java
+++ core/src/pjkck/dungeondaredevil/screens/ScrMap.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -13,6 +15,7 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import pjkck.dungeondaredevil.GamDungeonDaredevil;
 
 public class ScrMap implements Screen {
+    Sprite sprite;
 
     private GamDungeonDaredevil game;
 
@@ -23,17 +26,22 @@ public class ScrMap implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+
     public ScrMap(GamDungeonDaredevil game) {
         this.game = game;
 
         cam = new OrthographicCamera();
-        port = new FitViewport(1920, 1080, cam);
+        port = new FitViewport(640, 360, cam);
         cam.position.set(port.getWorldWidth() / 2, port.getWorldHeight() / 2, 0);
 
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("map.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
+        sprite = new Sprite(new Texture("player.jpg"));
+        sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2,
+                Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
     }
+
 
     @Override
     public void show() {
@@ -66,11 +74,22 @@ public class ScrMap implements Screen {
         update();
 
         renderer.setView(cam);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            sprite.translateX(-5f);
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            sprite.translateX(5f);
+        if(Gdx.input.isKeyPressed(Input.Keys.UP))
+            sprite.translateY(5f);
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+            sprite.translateY(-5f);
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         renderer.render();
+        game.getBatch().begin();
+        game.getBatch().draw(sprite, sprite.getX(), sprite.getY());
+        game.getBatch().end();
     }
 
     @Override
